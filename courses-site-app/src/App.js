6888401ca0225355cfff1daafbbaeb9b8de18167
Asier <asierp.dev@gmail.com>
2018-08-23 19:14:21 +0200

    create courses

--- courses-site-app/src/App.js
+++ courses-site-app/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
+<<<<<<< HEAD
 import { Route, HashRouter } from 'react-router-dom';
 import ReactDOM from "react-dom";
 import TopBar from "./js/components/TopBar/TopBar";
 import Home from "./js/components/Home/Home";
 import Footer from "./js/components/Footer/Footer"
 import Contact from "./js/components/Contact/Contact"
-import AdminFrame from "./js/components/Admin/AdminFrame/AdminFrame"
-import Students from "./js/components/Admin/Students/Students"
-import Teachers from "./js/components/Admin/Teachers/Teachers"
-import Courses from "./js/components/Admin/Courses/Courses"
-import ManageCourses from "./js/components/Admin/Courses/ManageCourses/ManageCourses"
-import ManageStudents from "./js/components/Admin/Students/ManageStudents/ManageStudents"
 import CoursesGridView from "./js/components/CoursesGridView/index"
 import CourseDetails from "./js/components/CourseView/index"
 import Checkout from './js/components/Checkout/index'
+=======
+import ReactDOM from "react-dom";
+import HelloWorld from "./js/components/HelloWorld"
+import CoursesGrid from './js/components/CoursesGrid'
+>>>>>>> a5e05e63ea55b46e02f8988499a89263e3fcf1ab
 
 class App extends Component {
   constructor() {
@@ -23,50 +23,34 @@ class App extends Component {
   }
   render() {
     return (
+<<<<<<< HEAD
         <HashRouter>
           <div>
-            <Route path="/es" render={()=> (
-              <TopBar/>              
-            )} />
-            <Route exact path="/es/home" render={() => (
-              <Home />
-            )} />
-            <Route exact path="/es/contacto" render={() => (
-              <Contact />
-            )} />
-            <Route exact path="/es/cursos" render={() => (
+            <TopBar/>
+            <Route exact path="/" render={() => (
+                  <Home />
+                )} />
+            <Route exact path="/contacto" render={() => (
+                  <Contact />
+                )} />
+            <Route exact path="/cursos" render={() => (
               <CoursesGridView />
             )} />
-            <Route exact path="/es/detalles-curso" render={() => (
+            <Route exact path="/detalles-curso" render={() => (
               <CourseDetails />
             )} />
-            <Route exact path="/es/checkout" render={() => (
+            <Route exact path="/checkout" render={() => (
             <Checkout />
             )} />
-            <Route path="/es" render={()=> (
-              <Footer/>              
-            )} />
-            <Route path="/admin" render={() => (
-              <AdminFrame />
-            )} />
-            <Route exact path="/admin/students" render={() => (
-              <Students />
-            )} />
-            <Route exact path="/admin/teachers" render={() => (
-              <Teachers />
-            )} />
-            <Route exact path="/admin/courses" render={() => (
-              <Courses />
-            )} />
-            <Route exact path="/admin/courses/manage" render={() => (
-              <ManageCourses />
-            )} />
-            <Route exact path="/admin/students/manage" render={() => (
-              <ManageStudents />
-            )} />
-                         
-            </div>   
+            <Footer/>
+            </div>
         </HashRouter>
+=======
+      <div>
+          <HelloWorld />
+          <CoursesGrid />
+      </div>
+>>>>>>> a5e05e63ea55b46e02f8988499a89263e3fcf1ab
     );
   }
 }
